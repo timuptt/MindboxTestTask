b57d037092daab21170683c594aa86d7d9646f95
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-11-06 03:08:13 +0400

    Update query.sql

--- src/Task2/query.sql
+++ src/Task2/query.sql
@@ -7,5 +7,3 @@ LEFT JOIN
        products_db.products_to_categories ptc ON p.product_id = ptc.product_id
 LEFT JOIN
        products_db.categories c ON ptc.category_id = c.category_id;
-
--- Возможно, можно оптимизировать количество логических чтений с помощью индекса
